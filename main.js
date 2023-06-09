716a799e881f03f5c94cc2b7ecedb5224a5f9aa6
daw2-viri23 <virimurillojoseluis@fpllefia.com>
2023-06-09 14:37:48 +0200

    commit de los commits

---
+++ main.js
@@ -0,0 +1,5 @@
+console.log('hola');
+
+import { series } from "./src/datos/series";
+
+console.log(series);
---
+++ src/datos/series.js
@@ -0,0 +1,32 @@
+export const series = [
+  {
+    "nombre": "Stranger Things",
+    "año": 2016,
+    "descripcion": "Un grupo de niños se enfrenta a fenómenos sobrenaturales en un pequeño pueblo de los años 80.",
+    "imagen_url": "https://upload.wikimedia.org/wikipedia/en/6/6e/Stranger_Things_characters.png"
+  },
+  {
+    "nombre": "La Casa de Papel",
+    "año": 2017,
+    "descripcion": "Un enigmático personaje llamado 'El Profesor' recluta a ocho personas para llevar a cabo un ambicioso plan: atracar la Casa de la Moneda de España.",
+    "imagen_url": "https://i.blogs.es/7b5613/la-casa-de-papel-temporada-5/1366_2000.jpeg"
+  },
+  {
+    "nombre": "Black Mirror",
+    "año": 2011,
+    "descripcion": "Una serie antológica que explora la relación entre la tecnología y la sociedad, presentando historias oscuras y distópicas.",
+    "imagen_url": "https://itpeers.com/wp-content/uploads/2020/10/backmirror-scaled.jpg"
+  },
+  {
+    "nombre": "Breaking Bad",
+    "año": 2008,
+    "descripcion": "Un profesor de química diagnosticado con cáncer se convierte en un despiadado fabricante y vendedor de metanfetaminas para asegurar el futuro financiero de su familia.",
+    "imagen_url": "https://imagenes.20minutos.es/files/og_thumbnail/uploads/imagenes/2019/10/22/breakingBad.jpg"
+  },
+  {
+    "nombre": "The Crown",
+    "año": 2016,
+    "descripcion": "La serie sigue la vida de la Reina Isabel II y los eventos históricos que tuvieron lugar durante su reinado.",
+    "imagen_url": "https://www.elindependiente.com/wp-content/uploads/2021/08/the-crown-serie-netflix.jpg"
+  }
+]
